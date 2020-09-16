96a2e0527bf9d2c097941efc6970ab5c07019d06
Hyun2 <hks0284@gmail.com>
2020-09-17 00:42:43 +0900

    3/5 개념 정리

---
+++ 그리디 알고리즘/BOJ 5585.py
@@ -0,0 +1,12 @@
+# 잔돈은 500, 100, 50, 10, 1
+# 고객이 지불해야하는 금액은 입력으로 받은 n, 고객은 1000 지폐를 건냄
+# 이 때 가장 적은 거스름 돈의 개수를 구하시오.
+
+# 입력: 380
+# 출력: 4
+
+# don = 1000 - n
+# jandon_list.sort(reverse=True)
+# for jandon in jandon_list:
+# answer += don // jandon
+# don = don % jandon
